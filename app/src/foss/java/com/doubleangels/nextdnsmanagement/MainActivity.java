0a250aee26a426b415058c731b281ed36fd38490
doubleangels <matt@doubleangels.com>
2025-09-19 20:26:26 -0600

    Remove unused imports

--- app/src/foss/java/com/doubleangels/nextdnsmanagement/MainActivity.java
+++ app/src/foss/java/com/doubleangels/nextdnsmanagement/MainActivity.java
@@ -16,8 +16,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.CookieManager;
 import android.webkit.WebChromeClient;
-import android.webkit.WebChromeClient.CustomViewCallback;
-import android.webkit.WebChromeClient.FileChooserParams;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
